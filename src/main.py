a25c0bb349073eb68f68c9de6d3827e35a1f90f2
Erick Almeida <erickalmeida702@gmail.com>
2025-05-27 13:56:27 -0300

    fixing directory paths

--- src/main.py
+++ src/main.py
@@ -2,17 +2,17 @@ import asyncio
 import os
 import logging
 from motor.motor_asyncio import AsyncIOMotorClient
-from repositories.workorder_repository import TracOSWorkorderRepository
-from services.inbound_service import InboundService
-from services.outbound_service import OutboundService
+from src.repositories.workorder_repository import TracOSWorkorderRepository
+from src.services.inbound_service import InboundService
+from src.services.outbound_service import OutboundService
 
 logging.basicConfig(level=logging.INFO)
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 MONGO_DB = os.getenv("MONGO_DATABASE", "tractian")
 MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", "workorders")
-INBOUND_DIR = os.getenv("DATA_INBOUND_DIR", "data/inbound")
-OUTBOUND_DIR = os.getenv("DATA_OUTBOUND_DIR", "data/outbound")
+INBOUND_DIR = os.getenv("DATA_INBOUND_DIR", "../data/inbound")
+OUTBOUND_DIR = os.getenv("DATA_OUTBOUND_DIR", "../data/outbound")
 
 
 async def main():
