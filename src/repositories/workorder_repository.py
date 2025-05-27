1d23884ff1e97b1038bca99fa5c907facd45d5f7
Erick Almeida <erickalmeida702@gmail.com>
2025-05-27 12:52:29 -0300

    MongoDB repository abstraction with insert_many method

---
+++ src/repositories/workorder_repository.py
@@ -0,0 +1,32 @@
+from motor.motor_asyncio import AsyncIOMotorCollection
+from domain.models import TracOSWorkorder
+from datetime import datetime
+from pymongo import ReturnDocument
+
+
+class TracOSWorkorderRepository:
+    def __init__(self, collection: AsyncIOMotorCollection):
+        self.collection = collection
+
+    async def upsert(self, workorder: TracOSWorkorder) -> dict:
+        return await self.collection.find_one_and_update(
+            {"number": workorder.number},
+            {"$set": workorder.dict()},
+            upsert=True,
+            return_document=ReturnDocument.AFTER,
+        )
+
+    async def get_unsynced(self) -> list[dict]:
+        cursor = self.collection.find({"isSynced": False})
+        return await cursor.to_list(length=None)
+
+    async def mark_as_synced(self, number: int) -> None:
+        await self.collection.update_one(
+            {"number": number},
+            {
+                "$set": {
+                    "isSynced": True,
+                    "syncedAt": datetime.utcnow(),
+                }
+            }
+        )
